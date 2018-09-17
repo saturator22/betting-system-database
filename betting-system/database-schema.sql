99729773bf32b872637bdfdc4c064f58fce0d8fb
saturator22 <f.szczeski@gmail.com>
2018-09-17 13:57:57 +0200

    Implement database skeleton

--- betting-system/database-schema.sql
+++ betting-system/database-schema.sql
@@ -1,5 +1,7 @@
+-- DROP DATABASE betting_system;
 CREATE DATABASE betting_system;
 
+DROP SCHEMA box CASCADE;
 CREATE SCHEMA box;
 
 CREATE TABLE box.users (
@@ -11,6 +13,12 @@ CREATE TABLE box.users (
   balance INT DEFAULT 0
 );
 
+CREATE TABLE box.transcations (
+  user_id INT REFERENCES box.users(id),
+  date DATE DEFAULT now(),
+  value INT NOT NULL
+);
+
 CREATE TABLE box.locations (
   id INT PRIMARY KEY,
   name TEXT NOT NULL,
@@ -18,31 +26,45 @@ CREATE TABLE box.locations (
   address TEXT
 );
 
+CREATE TABLE box.boxer_teams (
+  id INT PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL
+);
+
+CREATE TABLE box.boxers (
+  id INT PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  nick_name TEXT
+);
+
+CREATE TABLE box.boxer_teams_boxers (
+  team_id INT REFERENCES box.boxer_teams(id),
+  boxer_id INT REFERENCES box.boxers(id)
+);
+
+CREATE TABLE box.competitors (
+  id INT PRIMARY KEY,
+  team_id INT REFERENCES box.boxer_teams(id),
+  boxer_id INT REFERENCES box.boxers(id)
+);
+
 CREATE TABLE box.event (
   id INT PRIMARY KEY,
   name TEXT NOT NULL,
   description TEXT,
   event_date DATE,
-  location_id INT REFERENCES box.locations(id)
+  location_id INT REFERENCES box.locations(id),
+  winner_id INT REFERENCES box.competitors(id)
 );
 
 CREATE TABLE box.bets (
   id INT PRIMARY KEY,
+  date DATE,
   user_id INT REFERENCES box.users(id),
   event_id INT REFERENCES box.event(id),
   bet INT NOT NULL
-);
-
-CREATE TABLE box.betting_history (
-  user_id INT REFERENCES box.users(id),
-  bet_id INT REFERENCES box.bets(id),
-  won BOOLEAN NOT NULL
-);
---TODO winner id references to ?
-CREATE TABLE box.event_history (
-  event_id INT REFERENCES box.event(id),
-  winner_id INT REFERENCES box.competitors(id),
-  won_competitor_1 BOOLEAN
 );
 
 CREATE TABLE box.rates (
@@ -50,5 +72,6 @@ CREATE TABLE box.rates (
   is_group_fight BOOLEAN,
   competitor_1 INT REFERENCES box.competitors(id),
   competitor_2 INT REFERENCES box.competitors(id),
-
-)
+  rate_1 REAL NOT NULL,
+  rate_2 REAL NOT NULL
+);
